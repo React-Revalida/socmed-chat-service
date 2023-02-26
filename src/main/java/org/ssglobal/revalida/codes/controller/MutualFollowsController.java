7092eca54f019c64f32d5c0cbbd497dbd4f4eb6b
brynpryl77 <brynpryl77@gmail.com>
2023-02-26 09:22:31 +0800

    renamed file

---
+++ src/main/java/org/ssglobal/revalida/codes/controller/MutualFollowsController.java
@@ -0,0 +1,27 @@
+package org.ssglobal.revalida.codes.controller;
+
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.ssglobal.revalida.codes.dto.AppUserDTO;
+import org.ssglobal.revalida.codes.service.MutualFollowsService;
+
+@RestController
+@RequestMapping(value = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
+public class MutualFollowsController {
+
+    @Autowired
+    private MutualFollowsService followsService;
+
+    @PostMapping("/mutual-followed")
+    public Set<AppUserDTO> getMutalFollows(@RequestHeader(name = HttpHeaders.AUTHORIZATION) String token) {
+        return followsService.mutualFollows(token);
+    }
+
+}
